d8ff847f8eb9ad5749b216d9f2813f39b46ad809
RBasic <rcham.pro@gmail.com>
2019-12-10 14:34:00 +0100

    FixedMovementPlatform

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
+    [SerializeField] private Vector3 destinationPos;
 
+    [Range(0, 5)][SerializeField] private float speed = 0;
+    private Vector3 originPos;
+    private float timer = 0;
+    private bool forward = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.position = new Vector3(transform.position.x,Mathf.PingPong(Time.time,6.5f )+ 1.5f, transform.position.z);
+        if(forward) timer += (Time.deltaTime * speed);
+        else timer -= (Time.deltaTime * speed);
+
+        if (timer >= 1) forward = false;
+        else if (timer <= 0) forward = true;
+        transform.position = Vector3.Lerp(originPos, destinationPos,timer);
+       //transform.position = new Vector3(transform.position.x,Mathf.PingPong(Time.time,6.5f )+ 1.5f, transform.position.z);
     }
 }
